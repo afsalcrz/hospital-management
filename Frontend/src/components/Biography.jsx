3a2d3accb1fb18a745e90d861e52655c1455dcb1
afsalcrz <afsalparudhur@gmail.com>
2025-01-04 07:32:05 +0530

    Done

--- Frontend/src/components/Biography.jsx
+++ Frontend/src/components/Biography.jsx
@@ -12,11 +12,11 @@ const Biography = ({imageUrl}) => {
         <p>BlueSky Hospital  is a state-of-the-art solution crafted to address the complexities of modern healthcare management. Designed to enhance operational efficiency and improve patient care, BlueSky HMS integrates innovative technologies with user-friendly interfaces, making it an indispensable tool for hospitals and healthcare institutions.
 
 At its core, BlueSky Hospital focuses on optimizing every aspect of hospital operations. It provides robust features for patient management, including seamless registration, appointment scheduling, and electronic medical records (EMR). This ensures that patient information is securely stored and easily accessible, facilitating better communication between healthcare providers and patients.</p>
-        <p>shanu love farhan</p>
-        <p>hhhhhhh</p>
-        <p>gggggggg</p>
-        <p>kkkkkkk</p>
-        <p>nnnnnnnnn</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
 
       </div>
       
