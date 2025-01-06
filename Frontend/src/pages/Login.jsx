8e47b003868c2e5dfdfc9877bb40a71d77b24954
shafna <shafnapt2003@gmail.com>
2025-01-06 08:15:27 +0500

    Done

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../main";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  return 
-    <div>
-      
+  const { isAuthenticated, setAuthenticated } = useContext(Context);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassward] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("",{},{withCredentials: true, headers:{"content-Type": "application/json"}})
+    } catch (error) {
+
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div className="container form-component login-form">
+      <h2>Sign In</h2>
+      <p>Pleas logon to continue</p>
+      <p>asdfghjkpoiuytrewqzxcvbnm</p>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassward(e.target.value)}
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="Confirm Password"
+        />
+        <div
+          style={{
+            gap: "10px",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+          }}
+        >
+          <p style={{ marginBottom: 0 }}>Not Registred?</p>
+          <Link
+            to={"/register"}
+            style={{ textDecoration: "none", alignItems: "center" }}
+          >
+            Register Now 
+          </Link>
+        </div>
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
-  
+  );
 };
 
 export default Login;
